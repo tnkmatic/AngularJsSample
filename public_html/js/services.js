6b3a9f25323124737b5ffb31fcffb46dc1681dcc
Eiichi <Eiichi@tnkmatic-spro3>
2015-07-21 20:30:04 +0900

    サービスの実装に着手(service.js)

--- public_html/js/services.js
+++ public_html/js/services.js
@@ -3,6 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/*******************************************************************************
+ * サービスの用途（ビジネスロジックの記述 or 汎用機能ユーティリティの記述）
+ * @param {type} module
+ * @returns {undefined}
+ ******************************************************************************/
 (function(module) {
     'use strict';
     //まだ未使用
